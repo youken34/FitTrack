768a080d5f4067ef879e5ef13a151a757f80fadc
Côme Bonal <come.bonal@gmail.com>
2025-09-10 14:58:10 +0200

    moving of the bcrypt packages

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
+  weight: { type: Number, required: false },
   targetWeight: { type: Number },
   createdAt: { type: Date, default: Date.now },
 });
