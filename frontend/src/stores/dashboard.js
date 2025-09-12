f0f2bdc6ba6e86e0586b1ba305ff7f7a76c3f192
Côme Bonal <come.bonal@gmail.com>
2025-09-12 17:42:06 +0200

    feat (reactive add of the weekly calories upon new session creation)

--- frontend/src/stores/dashboard.js
+++ frontend/src/stores/dashboard.js
@@ -28,15 +28,22 @@ export const useDashboardStore = defineStore("dashboard", {
       this.data.totalSessions++;
       this.data.hoursThisMonth =
         (this.data.hoursThisMonth || 0) + newData.duration / 60;
+
       if (!this.data.lastSessions) this.data.lastSessions = [];
-      this.data.lastSessions.push(newData.newSession);
-      this.data.lastSessions.sort(
-        (a, b) => new Date(b.date) - new Date(a.date)
+      this.data.lastSessions = [...this.data.lastSessions, newData.newSession]
+        .sort((a, b) => new Date(b.date) - new Date(a.date))
+        .slice(0, 3);
+
+      // 🔹 Calories par jour
+      if (!this.data.sessionsByDay) this.data.sessionsByDay = Array(7).fill(0);
+      const sessionDate = new Date(newData.newSession.date);
+      const dayIndex = (sessionDate.getDay() + 6) % 7;
+
+      this.data.sessionsByDay = this.data.sessionsByDay.map((calories, i) =>
+        i === dayIndex
+          ? calories + (newData.newSession.calories || 0)
+          : calories
       );
-      this.data.lastSessions = this.data.lastSessions.slice(0, 3);
-    },
-    clear() {
-      this.data = null;
     },
   },
 });
